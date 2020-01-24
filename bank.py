b7a47d12dac74c16e1e7f286eaa5dbc6353281a6
alphae-nix <18laurent@gmail.com>
2020-01-24 11:01:05 +0100

    ajout du premier fichier

---
+++ bank.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.8
+#!-*-coding:utf-8-*
+
+
+from tkinter import *
+from tkinter import ttk
+
+page_principale = Tk()
+page_principale.title("bank")
+frame = Frame(page_principale)
+
+
+
+page_principale.mainloop()
